ae6e3035b1066018195583212eb19c27363af2f9
lmtreser <lmtreser@gmail.com>
2024-10-24 10:47:10 -0300

    table ui

---
+++ server/nodered/tablas/datos_a_insertar.js
@@ -0,0 +1,27 @@
+let datos =
+[
+    {
+        time: 24102024,
+        temperature: 38.5,
+        humidity: 90
+    },
+    {
+        time: 24102024,
+        temperature:39.5,
+        humidity:92
+    },
+    {
+        time: 24102024,
+        temperature:37.0,
+        humidity:89
+    },
+    {
+        time: 24102024,
+        temperature:37.0,
+        humidity:89
+    }
+];
+
+msg.payload = datos;
+
+return msg;
